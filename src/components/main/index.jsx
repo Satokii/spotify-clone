cd2b1c56f875138c6f18c178808927179ed3a4fb
Satoki <satokiito@gmail.com>
2023-12-28 00:19:06 +0000

    added Main components, refactored code so fetch request functions are in Main

---
+++ src/components/main/index.jsx
@@ -0,0 +1,116 @@
+import { useState } from "react";
+import axios from "axios";
+
+import './styles/main.css'
+
+function Main({ token }) {
+
+    const [topTracks, setTopTracks] = useState([]);
+    const [recentlyPlayed, setRecentlyPlayed] = useState([]);
+    const [playCount, setPlayCount] = useState([]);
+
+    const getTopTracks = async (e) => {
+        e.preventDefault();
+        const { data } = await axios.get(
+          "https://api.spotify.com/v1/me/top/tracks",
+          {
+            headers: {
+              Authorization: `Bearer ${token}`,
+            },
+            params: {
+              time_range: "long_term",
+              limit: 10,
+            },
+          }
+        );
+        // console.log(data)
+        setTopTracks(data.items);
+      };
+    
+      const renderTopTracks = () => {
+        return topTracks.map((track) => (
+          <div key={track.id}>
+            <p>
+              Song: {track.name} by: {track.artists[0].name}
+            </p>
+          </div>
+        ));
+      };
+    
+      const getRecentlyPlayed = async (e) => {
+        e.preventDefault();
+        const { data } = await axios.get(
+          "https://api.spotify.com/v1/me/player/recently-played",
+          {
+            headers: {
+              Authorization: `Bearer ${token}`,
+            },
+            params: {
+              limit: 50,
+            },
+          }
+        );
+        console.log(data.items);
+        setRecentlyPlayed(data.items);
+      };
+    
+      const renderRecentlyPlayed = () => {
+        return recentlyPlayed.map((track) => (
+          <div key={track.track.id}>
+            <p>Song: {track.track.name}</p>
+          </div>
+        ));
+      };
+    
+      const calcTimesPlayed = () => {
+        const newArr = [];
+        recentlyPlayed.forEach((track) => {
+          const checkDups = newArr.find(
+            (updatedTrack) => updatedTrack.track.id === track.track.id
+          );
+          if (checkDups) {
+            checkDups.timesPlayed += 1;
+          } else {
+            const updated = { ...track, timesPlayed: 1 };
+            newArr.push(updated);
+          }
+        });
+        console.log(newArr);
+        setPlayCount(newArr);
+      };
+    
+      const renderPlayCount = () => {
+        return playCount.map((track) => (
+          <div key={track.id}>
+            <p>
+              Song: {track.track.name}, times played: {track.timesPlayed}, time
+              spent listening:{" "}
+              {new Date(track.timesPlayed * track.track.duration_ms)
+                .toISOString()
+                .substring(11, 19)}
+            </p>
+          </div>
+        ));
+      };
+
+    return (
+        <main className="main grid">
+          <div>
+            <button onClick={getTopTracks}>Get Top Tracks</button>
+            {renderTopTracks()}
+          </div>
+          <hr />
+          <div>
+            <button onClick={getRecentlyPlayed}>Get Recently Played</button>
+            {renderRecentlyPlayed()}
+          </div>
+          <hr />
+          <div>
+            <button onClick={calcTimesPlayed}>Times played</button>
+            {renderPlayCount()}
+          </div>
+        </main>
+    )
+}
+
+export default Main
