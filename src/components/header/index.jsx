bf0685d273b7292673eef3787b33c9165e392b7b
Satoki <satokiito@gmail.com>
2023-12-30 23:37:22 +0000

    simple logo and styling added to header

--- src/components/header/index.jsx
+++ src/components/header/index.jsx
@@ -1,5 +1,7 @@
 import Authorisation from "../../Authorisation";
 import Logout from "../../Logout";
+import SpotifyLogo from "../../assets/svgs/spotify-logo.svg"
+
 import "./styles/header.css";
 
 function Header({ token, setToken }) {
@@ -11,7 +13,10 @@ function Header({ token, setToken }) {
 
   return (
     <header className="header grid">
-      <section className="logo-container">Spoti-facts</section>
+      <section className="logo--container grid">
+        <img className="logo--img" src={SpotifyLogo} alt="spotify logo" />
+        <h1 className="logo--text">Spoti-facts</h1>
+      </section>
       <section className="profile-container">Profile</section>
       <section className="login-container">
         {!token ? <Authorisation /> : <p onClick={logout}>Logout</p>}
