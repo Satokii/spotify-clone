a623296348677dfcbc134bc25b79eb82b2b43fed
Satoki <satokiito@gmail.com>
2023-12-30 00:26:31 +0000

    changed design from dropdown menus to static options for date ranges

--- src/components/main/index.jsx
+++ src/components/main/index.jsx
@@ -1,4 +1,4 @@
-import { useState, useRef, useEffect } from "react";
+import { useState } from "react";
 import TopTracksPreview from "./components/TopTracksPreview";
 import TopTracksPview6Mths from "./components/TopTracksPview6Mths";
 import TopTracksPview4Wks from "./components/TopTracksPview4Wks";
@@ -9,163 +9,126 @@ import RecentlyPlayed from "./components/RecentlyPlayed";
 import "./styles/main.css";
 
 function Main({ token }) {
-  const TRACKS_INITIAL_STATE = "long";
-  const ARTISTS_INITIAL_STATE = "long";
 
-  const [topTracks, setTopTracks] = useState(TRACKS_INITIAL_STATE);
-  const [topArtists, setTopArtists] = useState(ARTISTS_INITIAL_STATE);
-
-  // TOGGLE TRACKS DROPDOWN
-  const toggleTopTracks = () => {
-    if (topTracks === "long") return <TopTracksPreview token={token} />;
-    else if (topTracks === "medium")
-      return <TopTracksPview6Mths token={token} />;
-    else if (topTracks === "short") return <TopTracksPview4Wks token={token} />;
-  };
-
-  const [showTopTracksDropdown, setShowTopTracksDropdown] = useState(false);
-  const tracksMenuRef = useRef(null);
-  const tracksButtonRef = useRef(null);
-
-  const toggleTopTracksDropdown = () => {
-    setShowTopTracksDropdown(!showTopTracksDropdown);
-  };
-
-  const handleOutsideClickTracksDropdown = (e) => {
-    if (
-      tracksMenuRef.current &&
-      !tracksMenuRef.current.contains(e.target) &&
-      e.target !== tracksButtonRef.current
-    ) {
-      setShowTopTracksDropdown(false);
+  // HANDLE TOP TRACKS DATE RANGE
+  const TRACKS_INITIAL_STATE = [
+    {
+      name: 'ALL TIME',
+      className: 'active-date-filter',
+      click: 'long'
+    },
+    {
+      name: 'LAST 6 MONTHS',
+      className: 'inactive-date-filter',
+      click: 'medium'
+    },
+    {
+      name: 'LAST 4 WEEKS',
+      className: 'inactive-date-filter',
+      click: 'short'
     }
+  ];
+
+  const [showTopTracks, setShowTopTracks] = useState('long')
+  const [topTracksDate, setTopTracksDate] = useState(TRACKS_INITIAL_STATE);
+
+  const toggleTopTracksDate = (selectedDate) => {
+    const updatedTopTracks = topTracksDate.map(date => {
+      if (date.name === selectedDate.target.innerText) {
+        return {...date, className: 'active-date-filter'}
+      }
+      else {
+        return {...date, className:'inactive-date-filter'}
+      }
+    })
+    setTopTracksDate(updatedTopTracks)
+  }
+
+  const toggleTopTracks = () => {
+    if (showTopTracks === "long") return <TopTracksPreview token={token} />;
+    else if (showTopTracks === "medium")
+      return <TopTracksPview6Mths token={token} />;
+    else if (showTopTracks === "short") return <TopTracksPview4Wks token={token} />;
   };
 
-  useEffect(() => {
-    document.addEventListener("mousedown", handleOutsideClickTracksDropdown);
-    return () => {
-      document.removeEventListener(
-        "mousedown",
-        handleOutsideClickTracksDropdown
-      );
-    };
-  }, []);
+  // HANDLE TOP ARTISTS DATE RANGE
+  const ARTISTS_INITIAL_STATE = [
+    {
+      name: 'ALL TIME',
+      className: 'active-date-filter',
+      click: 'long'
+    },
+    {
+      name: 'LAST 6 MONTHS',
+      className: 'inactive-date-filter',
+      click: 'medium'
+    },
+    {
+      name: 'LAST 4 WEEKS',
+      className: 'inactive-date-filter',
+      click: 'short'
+    }
+  ];
+  
+  const [showTopArtists, setShowTopArtists] = useState('long')
+  const [topArtistsDate, setTopArtistsDate] = useState(ARTISTS_INITIAL_STATE);
 
-  // TOGGLE ARTISTS DROPDOWN
+  const toggleTopArtistsDate = (selectedDate) => {
+    const updatedTopArtists = topArtistsDate.map(date => {
+      if (date.name === selectedDate.target.innerText) {
+        return {...date, className: 'active-date-filter'}
+      }
+      else {
+        return {...date, className:'inactive-date-filter'}
+      }
+    })
+    setTopArtistsDate(updatedTopArtists)
+  }
+
   const toggleTopArtists = () => {
-    if (topArtists === "long") return <TopArtistsPreview token={token} />;
-    else if (topArtists === "medium")
+    if (showTopArtists === "long") return <TopArtistsPreview token={token} />;
+    else if (showTopArtists === "medium")
       return <TopArtistsPview6mths token={token} />;
-    else if (topArtists === "short")
+    else if (showTopArtists === "short")
       return <TopArtistsPview4Wks token={token} />;
   };
-
-  const [showTopArtistsDropdown, setShowTopArtistsDropdown] = useState(false);
-  const artistsMenuRef = useRef(null);
-  const artistsButtonRef = useRef(null);
-
-  const toggleTopArtistsDropdown = () => {
-    setShowTopArtistsDropdown(!showTopArtistsDropdown);
-  };
-
-  const handleOutsideClickArtistsDropdown = (e) => {
-    if (
-      artistsMenuRef.current &&
-      !artistsMenuRef.current.contains(e.target) &&
-      e.target !== artistsButtonRef.current
-    ) {
-      setShowTopArtistsDropdown(false);
-    }
-  };
-
-  useEffect(() => {
-    document.addEventListener("mousedown", handleOutsideClickArtistsDropdown);
-    return () => {
-      document.removeEventListener(
-        "mousedown",
-        handleOutsideClickArtistsDropdown
-      );
-    };
-  }, []);
 
   return (
     <main className="main grid">
       <section className="top-tracks--container grid">
         <h2 className="top-tracks--header">Top Tracks Preview</h2>
-        <button
-          className="date-filter-btn btn grid"
-          ref={tracksButtonRef}
-          onClick={() => toggleTopTracksDropdown()}
-        >
-          Filter Date Range
-        </button>
-        {showTopTracksDropdown && (
-          <ul className="date-filter-list" ref={tracksMenuRef}>
-            <li
-              onClick={() => {
-                setShowTopTracksDropdown(false);
-                setTopTracks("long");
-              }}
-            >
-              All Time
-            </li>
-            <li
-              onClick={() => {
-                setShowTopTracksDropdown(false);
-                setTopTracks("medium");
-              }}
-            >
-              Last 6 Months
-            </li>
-            <li
-              onClick={() => {
-                setShowTopTracksDropdown(false);
-                setTopTracks("short");
-              }}
-            >
-              Last 4 Weeks
-            </li>
+          <ul className="date-filter-list grid">
+            {topTracksDate.map((dateFilter, index) => 
+              <li
+                key={`${dateFilter.title}-${index}`}
+                className={dateFilter.className}
+                onClick={e => {
+                  toggleTopTracksDate(e)
+                  setShowTopTracks(dateFilter.click)
+                }}
+              >
+                {dateFilter.name}
+              </li>
+            )}
           </ul>
-        )}
         {toggleTopTracks()}
       </section>
       <section className="top-artists--container grid">
         <h2 className="top-artists--header">Top Artists Preview</h2>
-        <button
-          className="date-filter-btn btn grid"
-          ref={artistsButtonRef}
-          onClick={() => toggleTopArtistsDropdown()}
-        >
-          Filter Date Range
-        </button>
-        {showTopArtistsDropdown && (
-          <ul className="date-filter-list" ref={artistsMenuRef}>
-            <li
-              onClick={() => {
-                setShowTopArtistsDropdown(false);
-                setTopArtists("long");
-              }}
-            >
-              All Time
-            </li>
-            <li
-              onClick={() => {
-                setShowTopArtistsDropdown(false);
-                setTopArtists("medium");
-              }}
-            >
-              Last 6 Months
-            </li>
-            <li
-              onClick={() => {
-                setShowTopArtistsDropdown(false);
-                setTopArtists("short");
-              }}
-            >
-              Last 4 Weeks
-            </li>
+          <ul className="date-filter-list grid">
+            {topArtistsDate.map((dateFilter, index) => 
+              <li
+                key={`${dateFilter.title}-${index}`}
+                className={dateFilter.className}
+                onClick={e => {
+                  toggleTopArtistsDate(e)
+                  setShowTopArtists(dateFilter.click)
+                }}
+              >
+                {dateFilter.name}
+              </li>
+            )}
           </ul>
-        )}
         {toggleTopArtists()}
       </section>
       <RecentlyPlayed token={token} />
