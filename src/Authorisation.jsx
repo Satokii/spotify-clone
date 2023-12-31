07fd31600ac3f55fc4507f776b503d2640242d3b
Satoki <satokiito@gmail.com>
2023-12-31 00:41:24 +0000

    added basic styling to login and logout header buttons

--- src/Authorisation.jsx
+++ src/Authorisation.jsx
@@ -1,16 +1,18 @@
 function Authorisation() {
-    const CLIENT_ID = "04e1410dfac14d27968c7ef88c68d41c";
-    const REDIRECT_URI = "http://localhost:5173/";
-    const AUTH_ENDPOINT = "https://accounts.spotify.com/authorize";
-    const SCOPE = "user-top-read user-read-recently-played";
-    const RESPONSE_TYPE = "token";
-    const SHOW_DIALOG = true;
+  const CLIENT_ID = "04e1410dfac14d27968c7ef88c68d41c";
+  const REDIRECT_URI = "http://localhost:5173/";
+  const AUTH_ENDPOINT = "https://accounts.spotify.com/authorize";
+  const SCOPE = "user-top-read user-read-recently-played";
+  const RESPONSE_TYPE = "token";
+  const SHOW_DIALOG = true;
 
-    return (
-        <a
-            href={`${AUTH_ENDPOINT}?client_id=${CLIENT_ID}&scope=${SCOPE}&redirect_uri=${REDIRECT_URI}&response_type=${RESPONSE_TYPE}&show_dialog=${SHOW_DIALOG}`}
-          >Login to Spotify</a>
-    )
+  return (
+    <a
+      href={`${AUTH_ENDPOINT}?client_id=${CLIENT_ID}&scope=${SCOPE}&redirect_uri=${REDIRECT_URI}&response_type=${RESPONSE_TYPE}&show_dialog=${SHOW_DIALOG}`}
+    >
+      Login to Spotify
+    </a>
+  );
 }
 
-export default Authorisation
+export default Authorisation;
--- src/components/header/index.jsx
+++ src/components/header/index.jsx
@@ -3,6 +3,7 @@ import Logout from "../../Logout";
 import Logo from "./components/Logo";
 
 import "./styles/header.css";
+import "./styles/login-logout.css"
 
 function Header({ token, setToken }) {
   const logout = () => {
@@ -22,8 +23,8 @@ function Header({ token, setToken }) {
         </ul>
       </section>
       <section className="profile-container">Profile</section>
-      <section className="login-container">
-        {!token ? <Authorisation /> : <p onClick={logout}>Logout</p>}
+      <section className="header--login-logout-container">
+        {!token ? <Authorisation /> : <p className="header--logout-link" onClick={logout}>Logout</p>}
       </section>
     </header>
   );
