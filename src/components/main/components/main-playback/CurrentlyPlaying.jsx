9366c71a6143e3ec2ad6f5af23132c37b5f760da
Satoki <satokiito@gmail.com>
2024-01-01 14:33:53 +0000

    created component for currently playing section and css file

---
+++ src/components/main/components/main-playback/CurrentlyPlaying.jsx
@@ -0,0 +1,12 @@
+import "../../styles/main-playback/currently-playing.css"
+
+function CurrentlyPlaying() {
+
+    return (
+        <div className="main-playback--cur-playing-container grid">
+            <h3>Currently Playing</h3>
+        </div>
+    )
+}
+
+export default CurrentlyPlaying
--- src/components/main/components/main-playback/MainPlayback.jsx
+++ src/components/main/components/main-playback/MainPlayback.jsx
@@ -1,12 +1,12 @@
+import CurrentlyPlaying from "./CurrentlyPlaying"
+
 import "../../styles/main-playback/main-playback.css"
 
 function MainPlayback() {
 
     return (
         <section className="main-playback--container grid">
-            <div className="main-playback--cur-playing-container">
-                <h3>Currently Playing</h3>
-            </div>
+            <CurrentlyPlaying />
             <div>
                 <h3>Queue</h3>
                 <p>Up next:</p>
