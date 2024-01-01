9051f013f6196f220967ffe2d6df1da5eba94c60
Satoki <satokiito@gmail.com>
2024-01-01 18:52:48 +0000

    added code to fetch user queue from api

--- src/components/main/components/main-playback/Queue.jsx
+++ src/components/main/components/main-playback/Queue.jsx
@@ -1,9 +1,27 @@
-import { useEffect } from "react";
+import { useEffect, useState } from "react";
 import axios from "axios";
 
 import "../../styles/main-playback/queue.css"
 
 function Queue({ token }) {
+
+    const [queue, setQueue] = useState([])
+
+    useEffect(() => {
+        const getQueue = async () => {
+          const { data } = await axios.get(
+            "https://api.spotify.com/v1/me/player/queue",
+            {
+              headers: {
+                Authorization: `Bearer ${token}`,
+              }
+            }
+          );
+          console.log(data.queue)
+          setQueue(data.queue)
+        };
+        getQueue();
+      }, [token]);
 
     return (
         <div className="main-playback--queue-container grid">
