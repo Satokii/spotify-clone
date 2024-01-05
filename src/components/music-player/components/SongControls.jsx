56e23e565af373223f9d88284295e0170b68dd26
Satoki <satokiito@gmail.com>
2024-01-05 04:02:26 +0000

    extracted SongControls out to separate component

---
+++ src/components/music-player/components/SongControls.jsx
@@ -0,0 +1,37 @@
+import skipTrack from "../functions/skipTrack";
+import changePlayerState from "../functions/changePlayerState";
+
+import forwardButton from "../../../assets/svgs/player/forward-button.svg";
+import backButton from "../../../assets/svgs/player/back-button.svg";
+import playButton from "../../../assets/svgs/player/play-button.svg";
+import pauseButton from "../../../assets/svgs/player/pause-button.svg";
+
+function SongControls({ token, currentTrack, setCurrentTrack}) {
+
+  const togglePlayBtn = currentTrack.trackIsPlaying ? pauseButton : playButton;
+
+  return (
+    <div className="song-controls grid">
+      <div
+        className="back-btn"
+        onClick={() => skipTrack(token, currentTrack, "previous")}
+      >
+        <img src={backButton} alt="skip back button" />
+      </div>
+      <div
+        className="play-btn"
+        onClick={() => changePlayerState(token, currentTrack, setCurrentTrack)}
+      >
+        <img src={togglePlayBtn} alt="play/pause button" />
+      </div>
+      <div
+        className="forward-btn"
+        onClick={() => skipTrack(token, currentTrack, "next")}
+      >
+        <img src={forwardButton} alt="skip forward button" />
+      </div>
+    </div>
+  );
+}
+
+export default SongControls;
--- src/components/music-player/index.jsx
+++ src/components/music-player/index.jsx
@@ -1,14 +1,8 @@
 import { useEffect, useState } from "react";
-import changePlayerState from "./functions/changePlayerState";
-import skipTrack from "./functions/skipTrack";
 import seekToPosition from "./functions/seekToPosition";
 import getQueue from "../Queue/functions/getQueue";
 import TrackSlider from "./components/TrackSlider";
-
-import playButton from "../../assets/svgs/player/play-button.svg";
-import pauseButton from "../../assets/svgs/player/pause-button.svg";
-import forwardButton from "../../assets/svgs/player/forward-button.svg";
-import backButton from "../../assets/svgs/player/back-button.svg";
+import SongControls from "./components/SongControls";
 
 import "./styles/music-player.css";
 
@@ -17,7 +11,6 @@ function MusicPlayer({ token, currentTrack, setCurrentTrack, setQueue }) {
   const [sliderVal, setSliderVal] = useState(0);
   const [manualSeekVal, setManualSeekVal] = useState();
 
-  const togglePlayBtn = currentTrack.trackIsPlaying ? pauseButton : playButton;
   const timeElapsed = Number(((currentTrack.trackProgress / currentTrack.trackDuration) * 100).toFixed(0));
 
   useEffect(() => {
@@ -37,25 +30,7 @@ function MusicPlayer({ token, currentTrack, setCurrentTrack, setQueue }) {
 
   return (
     <section className="song-player-container grid">
-      <div className="song-controls grid">
-        <div
-          className="back-btn"
-          onClick={() => skipTrack(token, currentTrack, "previous")}
-        >
-          <img src={backButton} alt="skip back button" />
-        </div>
-        <div
-          className="play-btn"
-          onClick={() =>
-            changePlayerState(token, currentTrack, setCurrentTrack)
-          }
-        >
-          <img src={togglePlayBtn} alt="play/pause button" />
-        </div>
-        <div className="forward-btn" onClick={() => skipTrack(token, currentTrack, "next")}>
-          <img src={forwardButton} alt="skip forward button" />
-        </div>
-      </div>
+      <SongControls token={token} currentTrack={currentTrack} setCurrentTrack={setCurrentTrack}/>
       <TrackSlider currentTrack={currentTrack} sliderVal={sliderVal} setSliderVal={setSliderVal} setManualSeekVal={setManualSeekVal} />
     </section>
   );
