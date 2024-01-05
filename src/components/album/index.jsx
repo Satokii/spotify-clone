e8037e91e340720e9c23cddf47e66fd55cbbabc0
Satoki <satokiito@gmail.com>
2024-01-05 18:59:09 +0000

    created main section divs for album page

--- src/components/album/index.jsx
+++ src/components/album/index.jsx
@@ -2,11 +2,14 @@ import { useEffect, useState } from "react";
 import axios from "axios";
 import { useParams } from "react-router-dom";
 
+import "./styles/album-page.css"
+
 function Album({ token }) {
 
     const [albumInfo, setAlbumInfo] = useState([])
-
     const { albumId } = useParams()
+
+    // const bg = document.querySelector(".album-page--container")
 
     useEffect(() => {
         const getAlbum = async () => {
@@ -28,8 +31,11 @@ function Album({ token }) {
       }, [albumId, token]);
 
     return (
-        <section>
-
+        <section className="album-page--container" style={{backgroundColor: "blue"}}>
+            <div className="album-page--banner"></div>
+            <div className="album-page--controls"></div>
+            <div className="album-page--tracks"></div>
+            <div className="album-page--more"></div>
         </section>
     )
 }
