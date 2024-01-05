668785a9b21b99e9f39834a56c3ba6f4722dfe1d
Satoki <satokiito@gmail.com>
2024-01-05 23:36:10 +0000

    adding back button to search results page for consistency

--- src/components/search-results-page/index.jsx
+++ src/components/search-results-page/index.jsx
@@ -2,6 +2,7 @@ import SearchResultsTracks from "./components/SearchResultsTracks"
 import SearchResultsArtists from "./components/SearchResultsArtists"
 import SearchResultsAlbums from "./components/SearchResultsAlbums"
 import SearchResultsPlaylists from "./components/SearchResultsPlaylists"
+import { Link } from "react-router-dom"
 
 import "./styles/search-results-page.css"
 
@@ -9,7 +10,15 @@ function SearchResultsPage({ trackResults, trackTotal, artistResults, artistTota
     
     return (
         <section className="search-results--container grid">
-            <h2 className="search-results--header">Search Results</h2>
+            <div className="search-results--header-container grid">
+                <Link
+                  className="top-results-page--back-btn btn"
+                  to="/"
+                >
+                  Go back
+              </Link>
+                <h2 className="search-results--header">Search Results</h2>
+            </div>
             <article className="search-results--content grid">
                 <SearchResultsTracks trackResults={trackResults} trackTotal={trackTotal} />
                 < SearchResultsArtists artistResults={artistResults} artistTotal={artistTotal} />
