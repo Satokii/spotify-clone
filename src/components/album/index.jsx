c68ada445ffdf61f24cfebd52ebef9d8b473e6b6
Satoki <satokiito@gmail.com>
2024-01-06 17:16:04 +0000

    exported AlbumBanner to separate component

--- src/components/album/index.jsx
+++ src/components/album/index.jsx
@@ -2,11 +2,10 @@ import { useEffect, useState } from "react";
 import { useParams } from "react-router-dom";
 import getAlbum from "./functions/getAlbum";
 import getArtist from "../artist/functions/getArtist";
-import convertMsToTime from "../../shared-functions/convertMsToTime";
-import albumTimeinMs from "../../shared-functions/albumTimeinMs";
-import getYear from "../../shared-functions/getYear";
 import sleep from "../../shared-functions/sleep";
 import dynamicGradient from "../../ColorThief/dynamicGradient";
+
+import AlbumBanner from "./components/AlbumBanner";
 
 import "./styles/album-page.css"
 
@@ -32,25 +31,7 @@ function Album({ token }) {
 
     return (
         <section className="album-page--container grid">
-            <div className="album-page--banner grid">
-                <div className="album-page--banner-img-container">
-                    {albumInfo.img ? <img className="album-page--img" src={albumInfo.img} alt={`${albumInfo.name}-image`} /> : <div className="album-page--img-none"></div>}
-                </div>
-                <div className="album-page--banner-info-container grid">
-                    <p className="album-page--album-type">{albumInfo.type}</p>
-                    <p className="album-page--album-name">{albumInfo.name}</p>
-                    <div className="album-page--album-overview-container grid">
-                        <div className="album-overview-artist-img-container">
-                            {artistInfo.img ? <img src={artistInfo.img} alt={`${artistInfo.name}-image`} /> : <div></div>}
-                        </div>
-                        <p className="album-overview-artist-name">{artistInfo.name}</p>
-                        <span className="middot">&middot;</span>
-                        <p className="album-overview-release-year"> {getYear(albumInfo.releaseDate)} </p>
-                        <span className="middot">&middot;</span>
-                        <p className="album-overview-total-tracks">{`${albumInfo.totalTracks} songs, ${convertMsToTime(albumTimeinMs(albumTracksArr))}`}</p>
-                    </div>
-                </div>
-            </div>
+            <AlbumBanner albumInfo={albumInfo} artistInfo={artistInfo} albumTracksArr={albumTracksArr} />
             <div className="album-page--controls"></div>
             <div className="album-page--tracks"></div>
             <div className="album-page--more"></div>
