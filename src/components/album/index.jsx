34f7e5dd8adf767afe07514f833fe283dc9d551a
Satoki <satokiito@gmail.com>
2024-01-07 02:01:00 +0000

    extracted album controls component to separate file

--- src/components/album/index.jsx
+++ src/components/album/index.jsx
@@ -6,17 +6,9 @@ import sleep from "../../shared-functions/sleep";
 import dynamicGradient from "../../ColorThief/dynamicGradient";
 
 import AlbumBanner from "./components/AlbumBanner";
-import MainPlayBtn from "../../assets/svgs/main-app/main-play-btn.svg"
-import MainPauseBtn from "../../assets/svgs/main-app/main-pause-btn.svg"
-import GreenShuffleBtn from "../../assets/svgs/main-app/shuffle-green.svg"
-import GrayShuffleBtn from "../../assets/svgs/main-app/shuffle-gray.svg"
-import GreenHeart from "../../assets/svgs/main-app/heart-green.svg"
-import GrayHeart from "../../assets/svgs/main-app/heart-gray.svg"
-import MenuDots from "../../assets/svgs/main-app/menu-dots.svg"
-
+import AlbumControls from "./components/AlbumControls";
 
 import "./styles/album-page.css"
-import "./styles/album-controls.css"
 
 function Album({ token }) {
 
@@ -38,43 +30,10 @@ function Album({ token }) {
       dynamicGradient(albumInfo);
     });
 
-    const [play, setPlay] = useState(false)
-    const [shuffle, setShuffle] = useState(false)
-    const [saved, setSaved] = useState(false)
-
-    const togglePlayAlbum = () => {
-      if (!play) setPlay(true)
-      else setPlay(false)
-    }
-
-    const toggleShuffle = () => {
-      if (!shuffle) setShuffle(true)
-      else setShuffle(false)
-    }
-
-    const toggleSaved = () => {
-      if (!saved) setSaved(true)
-      else setSaved(false)
-    }
-
     return (
         <section className="album-page--container grid">
             <AlbumBanner albumInfo={albumInfo} artistInfo={artistInfo} albumTracksArr={albumTracksArr} />
-            <div className="album-page--controls grid">
-              {play ?
-              <img className="album-page--pause-btn" src={MainPauseBtn} alt="pause btn green" onClick={togglePlayAlbum} />
-              : <img className="album-page--play-btn" src={MainPlayBtn} alt="play btn green" onClick={togglePlayAlbum} />
-              }
-              {shuffle ? 
-                <img className="album-page--shuffle-btn-green" src={GreenShuffleBtn} alt="shuffle btn green" onClick={toggleShuffle} />
-                : <img className="album-page--shuffle-btn-gray" src={GrayShuffleBtn} alt="shuffle btn gray" onClick={toggleShuffle} />
-              }
-              {saved ?
-                <img className="album-page--heart-green" src={GreenHeart} alt="heart green" onClick={toggleSaved} />
-                : <img className="album-page--heart-gray" src={GrayHeart} alt="heart gray" onClick={toggleSaved} />
-              }
-              <img className="album-page--menu-dots" src={MenuDots} alt="menu dots" />
-            </div>
+            <AlbumControls />
             <div className="album-page--tracks"></div>
             <div className="album-page--more grid"></div>
         </section>
