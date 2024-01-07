f7f9c4b513d248d6b5e38dc13eda59b29cf947aa
Satoki <satokiito@gmail.com>
2024-01-07 02:37:29 +0000

    album tracks header added

--- src/components/album/components/AlbumTracks.jsx
+++ src/components/album/components/AlbumTracks.jsx
@@ -1,9 +1,19 @@
+import TimeIcon from "../../../assets/svgs/main-app/time.svg"
+
 import "../styles/album-tracks.css"
 
 function AlbumTracks() {
 
     return (
-        <div className="album-page--tracks"></div>
+        <div className="album-page--tracks grid">
+            <div className="album-page--tracks-headers grid">
+                <p className="tracks-header--number">#</p>
+                <p className="tracks-header--title">Title</p>
+                <img className="tracks-header--time" src={TimeIcon} alt="time icon" />
+            </div>
+            <div className="album-page--tracks-tracks"></div>
+            <div className="album-page--tracks-copyrights"></div>
+        </div>
     )
 }
 
