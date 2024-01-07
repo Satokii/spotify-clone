2cbd4587c325031843010a471e03b89986d00af6
Satoki <satokiito@gmail.com>
2024-01-07 00:03:44 +0000

    imported menu dots svg

--- src/components/album/index.jsx
+++ src/components/album/index.jsx
@@ -12,6 +12,7 @@ import GreenShuffleBtn from "../../assets/svgs/main-app/shuffle-green.svg"
 import GrayShuffleBtn from "../../assets/svgs/main-app/shuffle-gray.svg"
 import GreenHeart from "../../assets/svgs/main-app/heart-green.svg"
 import GrayHeart from "../../assets/svgs/main-app/heart-gray.svg"
+import MenuDots from "../../assets/svgs/main-app/menu-dots.svg"
 
 
 import "./styles/album-page.css"
@@ -42,10 +43,11 @@ function Album({ token }) {
             <AlbumBanner albumInfo={albumInfo} artistInfo={artistInfo} albumTracksArr={albumTracksArr} />
             <div className="album-page--controls grid">
               <img className="album-page--play-btn" src={MainPlayBtn} alt="play btn green" />
-              <img className="album-page--shuffle-btn-gray" src={GrayShuffleBtn} alt="" />
-              {/* <img className="album-page--shuffle-btn-green" src={GreenShuffleBtn} alt="" /> */}
-              <img className="album-page--heart-gray" src={GrayHeart} alt="" />
-              <img className="album-page--heart-green" src={GreenHeart} alt="" />
+              <img className="album-page--shuffle-btn-gray" src={GrayShuffleBtn} alt="shuffle btn gray" />
+              {/* <img className="album-page--shuffle-btn-green" src={GreenShuffleBtn} alt="shuffle btn green" /> */}
+              <img className="album-page--heart-gray" src={GrayHeart} alt="heart gray" />
+              {/* <img className="album-page--heart-green" src={GreenHeart} alt="heart green" /> */}
+              <img className="album-page--menu-dots" src={MenuDots} alt="menu dots" />
             </div>
             <div className="album-page--tracks"></div>
             <div className="album-page--more grid"></div>
