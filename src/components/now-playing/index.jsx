7bf1a3a00abc4ef431a6e74593fa178c770e99dc
Satoki <satokiito@gmail.com>
2024-01-10 01:52:52 +0000

    added link to album when clicking now playing track image

--- src/components/now-playing/index.jsx
+++ src/components/now-playing/index.jsx
@@ -7,9 +7,9 @@ function NowPlaying({ currentTrack }) {
     
     return (
         <section className="now-playing--container grid">
-            <div className="now-playing--img">
+            <Link className="now-playing--img" to={`/album/${currentTrack.albumId}/${currentTrack.artistId}`} onClick={scrollToTop}>
                 {currentTrack.trackImageLength ? <img src={currentTrack.trackImage} alt={`${currentTrack.trackName}-image`} /> : <div></div>}
-            </div>
+            </Link>
             <div className="now-playing--text-container grid">
                 <div className="now-playing--track-name-container">
                     <Link className="now-playing--track-name" to={`/album/${currentTrack.albumId}/${currentTrack.artistId}`} onClick={scrollToTop} >{currentTrack.trackName}</Link>
