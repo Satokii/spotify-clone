17c09e0c76a47a7ddb84279468a69c019892a7b8
Satoki <satokiito@gmail.com>
2024-01-13 04:27:13 +0000

    added basic styling to sidebar currently playing track

--- src/components/sidebar/components/SidebarCurrentTrack.jsx
+++ src/components/sidebar/components/SidebarCurrentTrack.jsx
@@ -3,10 +3,10 @@ import "../styles/sidebar-current-track.css"
 function SidebarCurrentTrack({ currentTrack }) {
 
     return (
-        <div className="sidebar--current-track grid">
-            <img src={currentTrack.trackImage} alt="current track img" />
-            <div>{currentTrack.trackName}</div>
-            <div>{currentTrack.trackArtist}</div>
+        <div className="sidebar--current-track-container grid">
+            <img className="sidebar--current-track-img" src={currentTrack.trackImage} alt="current track img" />
+            <div className="sidebar--current-track-name">{currentTrack.trackName}</div>
+            <div className="sidebar--current-track-artist">{currentTrack.trackArtist}</div>
         </div>
     )
 }
