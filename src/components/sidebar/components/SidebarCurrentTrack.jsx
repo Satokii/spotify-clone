ccb7a4eeeff33a61b7a6253aa91c3d5a188c2047
Satoki <satokiito@gmail.com>
2024-01-13 04:39:57 +0000

    Links added to navigate to album and artist page when clicked in currently playing track on sidebar

--- src/components/sidebar/components/SidebarCurrentTrack.jsx
+++ src/components/sidebar/components/SidebarCurrentTrack.jsx
@@ -1,12 +1,17 @@
+import { Link } from "react-router-dom"
+import scrollToTop from "../../../shared-functions/scrollToTop"
+
 import "../styles/sidebar-current-track.css"
 
 function SidebarCurrentTrack({ currentTrack }) {
 
     return (
         <div className="sidebar--current-track-container grid">
-            <img className="sidebar--current-track-img" src={currentTrack.trackImage} alt="current track img" />
-            <div className="sidebar--current-track-name">{currentTrack.trackName}</div>
-            <div className="sidebar--current-track-artist">{currentTrack.trackArtist}</div>
+            <Link to={`/album/${currentTrack.albumId}/${currentTrack.artistId}`} onClick={scrollToTop}>
+                <img className="sidebar--current-track-img" src={currentTrack.trackImage} alt="current track img" />
+            </Link>
+            <Link className="sidebar--current-track-name" to={`/album/${currentTrack.albumId}/${currentTrack.artistId}`} onClick={scrollToTop}>{currentTrack.trackName}</Link>
+            <Link className="sidebar--current-track-artist" to={`/artist/${currentTrack.artistId}`} onClick={scrollToTop}>{currentTrack.trackArtist}</Link>
         </div>
     )
 }
