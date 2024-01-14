67ddfea3daa6e7bc2485b99e965af23bf04b45c0
Satoki <satokiito@gmail.com>
2024-01-14 23:14:34 +0000

    added basic page structure to dashboard

--- src/components/dashboard/index.jsx
+++ src/components/dashboard/index.jsx
@@ -1,9 +1,15 @@
+import "./styles/dashboard.css"
+
 function Dashboard() {
 
     return (
-        <main>
-            
+        <div className="scrollbar-dashboard">
+        <main className="dashboard grid">
+            <section>Top Menu</section>
+            <section>Banner</section>
+            <section>Sub-container content</section>
         </main>
+        </div>
     )
 }
 
