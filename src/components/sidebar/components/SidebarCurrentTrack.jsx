ac84ce07dbd8f3ca00b5ba415269c0bf7696c3b7
Satoki <satokiito@gmail.com>
2024-01-14 22:55:31 +0000

    added border radius to sidebar and slightly amended sizing

--- src/components/sidebar/components/SidebarCurrentTrack.jsx
+++ src/components/sidebar/components/SidebarCurrentTrack.jsx
@@ -8,11 +8,13 @@ function SidebarCurrentTrack({ currentTrack }) {
 
     return (
         <div className="sidebar--current-track-container grid">
-            <Link to={`/album/${currentTrack.albumId}/${currentTrack.artistId}`} onClick={scrollToTop}>
+            <Link className="sidebar--current-track-img-container" to={`/album/${currentTrack.albumId}/${currentTrack.artistId}`} onClick={scrollToTop}>
+                <div>
                 {currentTrack.trackImage ?
                     <img className="sidebar--current-track-img" src={currentTrack.trackImage} alt="current track img" />
                     : <div>Play a song on Spotify</div>
                 }
+                </div>
             </Link>
             <Link className="sidebar--current-track-name" to={`/album/${currentTrack.albumId}/${currentTrack.artistId}`} onClick={scrollToTop}>{fixLengthPreviews(currentTrack.trackName)}</Link>
             <Link className="sidebar--current-track-artist" to={`/artist/${currentTrack.artistId}`} onClick={scrollToTop}>{currentTrack.trackArtist}</Link>
