0586cce62cb583ab819f30c356249bce62e85cc8
Satoki <satokiito@gmail.com>
2024-01-14 23:33:58 +0000

    added forward and back svgs to main header

--- src/components/dashboard/components/DashboardMenu.jsx
+++ src/components/dashboard/components/DashboardMenu.jsx
@@ -1,9 +1,18 @@
+import BackArrow from "../../../assets/svgs/main-app/back-arrow.svg"
+import ForwardArrow from "../../../assets/svgs/main-app/forward-arrow.svg"
+
 import "../styles/dashboard-menu.css"
 
 function DashboardMenu() {
 
     return (
-        <section className="dashboard--menu grid">Top Menu</section>
+        <section className="dashboard--menu grid">
+            <div className="main-page-nav-container grid">
+                <img className="main-page-nav--back-arrow" src={BackArrow} alt="" />
+                <img className="main-page-nav--forward-arrow" src={ForwardArrow} alt="" />
+            </div>
+            <div></div>
+        </section>
     )
 }
 
