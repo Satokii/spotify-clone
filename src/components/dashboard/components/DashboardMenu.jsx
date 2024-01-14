350fc1a3f7558172d6d60964785432a20773df5e
Satoki <satokiito@gmail.com>
2024-01-14 23:50:55 +0000

    imported useNavigate to give buttons functionality

--- src/components/dashboard/components/DashboardMenu.jsx
+++ src/components/dashboard/components/DashboardMenu.jsx
@@ -1,3 +1,4 @@
+import { useNavigate } from "react-router-dom"
 import BackArrow from "../../../assets/svgs/main-app/back-arrow.svg"
 import ForwardArrow from "../../../assets/svgs/main-app/forward-arrow.svg"
 
@@ -5,11 +6,21 @@ import "../styles/dashboard-menu.css"
 
 function DashboardMenu() {
 
+    const navigate = useNavigate()
+
+    const pageBack = () => {
+        navigate(-1)
+    }
+
+    const pageForward = () => {
+        navigate(1)
+    }
+
     return (
         <section className="dashboard--menu grid">
             <div className="main-page-nav-container grid">
-                <img className="main-page-nav--back-arrow" src={BackArrow} alt="" />
-                <img className="main-page-nav--forward-arrow" src={ForwardArrow} alt="" />
+                <img className="main-page-nav--back-arrow" src={BackArrow} alt="" onClick={pageBack} />
+                <img className="main-page-nav--forward-arrow" src={ForwardArrow} alt="" onClick={pageForward} />
             </div>
             <div></div>
         </section>
