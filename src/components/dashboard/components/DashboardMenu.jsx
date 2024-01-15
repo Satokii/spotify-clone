4964ae7197eae2206db337dd9b40b69823813e4e
Satoki <satokiito@gmail.com>
2024-01-15 00:30:39 +0000

    added profile and logout button, added hover effects

--- src/components/dashboard/components/DashboardMenu.jsx
+++ src/components/dashboard/components/DashboardMenu.jsx
@@ -1,6 +1,8 @@
 import { useNavigate } from "react-router-dom"
 import BackArrow from "../../../assets/svgs/main-app/back-arrow.svg"
 import ForwardArrow from "../../../assets/svgs/main-app/forward-arrow.svg"
+import ProfileIcon from "../../../assets/svgs/main-app/profile-icon.svg"
+import LogOutBtn from "../../../assets/svgs/main-app/log-out.svg"
 
 import "../styles/dashboard-menu.css"
 
@@ -17,12 +19,19 @@ function DashboardMenu() {
     }
 
     return (
-        <section className="dashboard--menu grid">
-            <div className="main-page-nav-container grid">
-                <img className="main-page-nav--back-arrow" src={BackArrow} alt="" onClick={pageBack} />
-                <img className="main-page-nav--forward-arrow" src={ForwardArrow} alt="" onClick={pageForward} />
+        <section className="main-page--menu grid">
+            <div className="main-page--nav-container grid">
+                <div onClick={pageBack}>
+                    <img className="main-page--nav-back-arrow" src={BackArrow} alt="back arrow" />
+                </div>
+                <div onClick={pageForward}>
+                    <img className="main-page--nav-forward-arrow" src={ForwardArrow} alt="forward arrow"  />
+                </div>
             </div>
-            <div></div>
+            <div className="main-page--icons-container grid">
+                <img className="main-page--profile" src={ProfileIcon} alt="profile icon" />
+                <img className="main-page--log-out" src={LogOutBtn} alt="log out button" />
+            </div>
         </section>
     )
 }
