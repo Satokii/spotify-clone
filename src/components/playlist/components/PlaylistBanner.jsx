f258fceb4f0ca46a2051fc64538a1fd2a1fd10da
Satoki <satokiito@gmail.com>
2024-01-16 02:36:18 +0000

    created api call to get individual playlist info, used fake data to set state for data

---
+++ src/components/playlist/components/PlaylistBanner.jsx
@@ -0,0 +1,50 @@
+import { Link } from "react-router-dom";
+import scrollToTop from "../../../shared-functions/scrollToTop";
+import getYear from "../../../shared-functions/getYear";
+import convertMsToTime from "../../../shared-functions/convertMsToTime";
+import albumTimeinMs from "../../../shared-functions/albumTimeinMs";
+
+import "../styles/album-banner.css"
+
+function PlaylistBanner({ albumInfo, artistInfo, albumTracksArr, artistId }) {
+  return (
+    <div className="album-page--banner grid">
+      <div className="album-page--banner-img-container">
+        {albumInfo.img ? (
+          <img
+            className="album-page--img"
+            src={albumInfo.img}
+            alt={`${albumInfo.name}-image`}
+          />
+        ) : (
+          <div className="album-page--img-none"></div>
+        )}
+      </div>
+      <div className="album-page--banner-info-container grid">
+        <p className="album-page--album-type">{albumInfo.type}</p>
+        <p className="album-page--album-name">{albumInfo.name}</p>
+        <div className="album-page--album-overview-container grid">
+          <div className="album-overview-artist-img-container">
+            {artistInfo.img ? (
+              <img src={artistInfo.img} alt={`${artistInfo.name}-image`} />
+            ) : (
+              <div></div>
+            )}
+          </div>
+          <Link className="album-overview-artist-name" to={`/artist/${artistId}`} onClick={scrollToTop}>{artistInfo.name}</Link>
+          <span className="middot">&middot;</span>
+          <p className="album-overview-release-year">
+            {" "}
+            {getYear(albumInfo.releaseDate)}{" "}
+          </p>
+          <span className="middot">&middot;</span>
+          <p className="album-overview-total-tracks">{`${
+            albumInfo.totalTracks
+          } songs, ${convertMsToTime(albumTimeinMs(albumTracksArr))}`}</p>
+        </div>
+      </div>
+    </div>
+  );
+}
+
+export default PlaylistBanner;
--- src/components/playlist/index.jsx
+++ src/components/playlist/index.jsx
@@ -1,8 +1,39 @@
+import axios from "axios";
+import { useEffect, useState } from "react";
 import PlaylistTopNav from "./components/PlaylistTopNav"
 
 import "./styles/playlist-page.css"
 
-function Playlist({ setToken }) {
+function Playlist({ token, setToken }) {
+
+    const [playlistInfo, setPlaylistInfo] = useState({})
+    const [playlistTracks, setPlaylistTracks] = useState([])
+
+    useEffect(() => {
+        const getPlaylist = async () => {
+            const { data } = await axios.get(
+              `https://api.spotify.com/v1/playlists/3cEYpjA9oz9GiPac4AsH4n`,
+              {
+                headers: {
+                  Authorization: `Bearer ${token}`,
+                },
+              }
+            );
+            console.log(data)
+            setPlaylistTracks(data.tracks.items)
+            setPlaylistInfo({
+                name: data.name,
+                img: data.images[0].url,
+                description: data.description,
+                followers: data.followers.total,
+                type: data.type,
+                isPublic: data.public,
+                owner: data.owner.display_name
+            })
+        };
+        getPlaylist()
+    }, [token])
+
 
     return (
         <div className="scrollbar-playlist">
