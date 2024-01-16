ae12db1212b1792b06d7ba64690856ff2ecdb738
Satoki <satokiito@gmail.com>
2024-01-16 02:20:58 +0000

    added playlist top nav

--- src/components/playlist/index.jsx
+++ src/components/playlist/index.jsx
@@ -1,9 +1,13 @@
-function Playlist() {
+import PlaylistTopNav from "./components/PlaylistTopNav"
+
+import "./styles/playlist-page.css"
+
+function Playlist({ setToken }) {
 
     return (
         <div className="scrollbar-playlist">
             <section className="playlist-page--container grid">
-                <div className="playlist-page--top-nav">top nav</div>
+                <PlaylistTopNav setToken={setToken} />
                 <div className="playlist-page--banner">playlist banner</div>
                 <div className="playlist-page--sub-container grid">sub container</div>
             </section>
