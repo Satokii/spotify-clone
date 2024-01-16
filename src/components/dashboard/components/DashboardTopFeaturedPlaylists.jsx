09317c0b6b1a11420073317a22f934ff86e1f51b
Satoki <satokiito@gmail.com>
2024-01-16 01:32:15 +0000

    added styling to top playlists

--- src/components/dashboard/components/DashboardTopFeaturedPlaylists.jsx
+++ src/components/dashboard/components/DashboardTopFeaturedPlaylists.jsx
@@ -4,6 +4,8 @@ import { Link } from "react-router-dom";
 import scrollToTop from "../../../shared-functions/scrollToTop";
 import PlayGreen from "../../../assets/svgs/main-app/main-play-btn.svg"
 
+import "../styles/dashboard-top-playlists.css"
+
 function DashboardTopFeaturedPlaylists({ token }) {
 
     const [topPlaylists, setTopPlaylists] = useState([])
@@ -22,20 +24,23 @@ function DashboardTopFeaturedPlaylists({ token }) {
               }
             );
             setTopPlaylists(data.playlists.items)
-            console.log(data.playlists.items)
+            // console.log(data.playlists.items)
         };
         getTopFeaturedPlaylists()
     }, [token])
 
     return (
-        <div className="dashboard--top-featured-playlists">
-            <div className="dashboard--top-featured-playlists-header">Featured Playlists Where You Are</div>
-            <div className="dashboard--top-featured-playlists-list">
+        <div className="dashboard--top-featured-playlists grid">
+            <div className="dashboard--top-featured-playlists-header-container grid">
+                <div className="dashboard--top-featured-playlists-header">Featured Playlists Where You Are</div>
+                <div className="dashboard--top-featured-playlists-show-all">Show all</div>
+            </div>
+            <div className="dashboard--top-featured-playlists-list grid">
             {topPlaylists.map(playlist =>
                     <Link className="top-featured-playlist--item-container grid" key={playlist.id} to={``} onClick={scrollToTop} >
-                        <div className="top-featured-playlist--img">
+                        <div className="top-featured-playlist--img-container">
                             {playlist.images.length ?
-                            <div className="top-featured-playlist--img-container">
+                            <div className="top-featured-playlist--img">
                               <img src={playlist.images[0].url} alt={playlist.name} />
                               <img className="top-featured-playlist--play" src={PlayGreen} alt="play button" />
                             </div>
@@ -43,7 +48,7 @@ function DashboardTopFeaturedPlaylists({ token }) {
                             }
                         </div>
                         <div className="top-featured-playlist--name">{playlist.name}</div>
-                        <div className="more-by-artist--album-date">{playlist.description}</div>
+                        <div className="top-featured-playlist--description">{playlist.description}</div>
                     </Link>
                 )}
             </div>
