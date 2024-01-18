a39f6f60791cd230c6af414b7ec389afebd1a579
Satoki <satokiito@gmail.com>
2024-01-18 00:34:25 +0000

    added filter to artist and playlist banners to counteract any bright white backgrounds

--- src/components/artist/components/ArtistBanner.jsx
+++ src/components/artist/components/ArtistBanner.jsx
@@ -6,12 +6,14 @@ function ArtistBanner({ artistInfo }) {
 
     return (
         <div className="artist-page--banner-container grid" style={{ backgroundImage: `url(${artistInfo.img})`, backgroundPosition: "50% 30%", backgroundRepeat: "no-repeat", backgroundSize: "80%"}}>
+            <div className="artist-page--banner-filter grid">
             <div className="artist-banner--verified-container grid">
                 <img className="artist-banner--verified-img" src={VerifiedIcon} alt="verified icon" />
                 <p className="artist-banner--verified-text">Verified Artist</p>
             </div>
             <div className="artist-banner--artist-name grid ">{artistInfo.name}</div>
             <div className="artist-banner--artist-followers">{`${artistInfo.followers} followers`}</div>
+            </div>
         </div>
     )
 }
--- src/components/playlist/components/PlaylistBanner.jsx
+++ src/components/playlist/components/PlaylistBanner.jsx
@@ -8,6 +8,7 @@ function PlaylistBanner({ playlistInfo, playlistTracks }) {
   console.log(playlistInfo)
   return (
     <div className="playlist-page--banner grid" style={{ backgroundImage: `url(${playlistInfo.img})`, backgroundPosition: "50% 30%", backgroundRepeat: "no-repeat", backgroundSize: "80%"}}>
+      <div className="playlist-page--banner-filter grid">
         {playlistInfo.isPublic ?
             <div className="playlist-page--playlist-type">{`Public ${playlistInfo.type}`}</div>
             : <div className="playlist-page--playlist-type">{playlistInfo.type}</div>
@@ -20,6 +21,7 @@ function PlaylistBanner({ playlistInfo, playlistTracks }) {
             <p className="playlist-page--playlist-likes">{`${playlistInfo.followers} followers`}</p>
             <span className="middot">&middot;</span>
             <p className="playlist-overview-total-tracks"><span className="playlist-overview-song-num">{`${playlistInfo.totalTracks} songs`}</span>{`, ${convertMsToTime(playlistTimeinMs(playlistTracks))}`}</p>
+        </div>
         </div>
     </div>
   );
