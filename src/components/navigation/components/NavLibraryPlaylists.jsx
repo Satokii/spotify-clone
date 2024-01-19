b35962d7d69663857122aed8a22d36962717b196
Satoki <satokiito@gmail.com>
2024-01-19 01:39:26 +0000

    api call to fetch number of liked songs, rendered in playlists list

--- src/components/navigation/components/NavLibraryPlaylists.jsx
+++ src/components/navigation/components/NavLibraryPlaylists.jsx
@@ -1,7 +1,43 @@
+import axios from "axios";
+
+import "../styles/nav-library-playlists.css"
+import { useEffect, useState } from "react";
+
 function NavLibraryPlaylists({ token }) {
 
+    const [likeSongsLibrary, setLikedSongsLibrary] = useState({})
+
+    useEffect(() => {
+        const getLikedSongsLibrary = async () => {
+            const { data } = await axios.get(
+              `https://api.spotify.com/v1/me/tracks`,
+              {
+                headers: {
+                  Authorization: `Bearer ${token}`,
+                },
+              }
+            );
+              setLikedSongsLibrary({
+                numTracks: data.total
+              })
+        };
+        getLikedSongsLibrary()
+    }, [token])
+
     return (
-        <div className='navigation--playlists'></div>
+        <div className='navigation--playlists-container'>
+            <div className='navigation--playlists-scrollbar'>
+                <ul className="navigation--playlists-list">
+                    <li className="navigation--playlists-liked-container">
+                        <img src="" alt="" />
+                        <div>Liked Songs</div>
+                        <div>Playlist</div>
+                        <span className="middot">&middot;</span>
+                        <div>{`${likeSongsLibrary.numTracks} songs`}</div>
+                    </li>
+                </ul>
+            </div>
+        </div>
     )
 }
 
