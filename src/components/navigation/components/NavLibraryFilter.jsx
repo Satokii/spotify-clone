5fb7b7580032696a431a795385b7a464a615fd13
Satoki <satokiito@gmail.com>
2024-01-19 00:41:45 +0000

    added styling to nav filter icon/headers

--- src/components/navigation/components/NavLibraryFilter.jsx
+++ src/components/navigation/components/NavLibraryFilter.jsx
@@ -1,4 +1,6 @@
 import { NavLink } from "react-router-dom"
+import CrossGray from "../../../assets/svgs/main-app/cross-gray.svg"
+import CrossBlack from "../../../assets/svgs/main-app/cross-black.svg"
 
 import "../styles/nav-library-filter.css"
 
@@ -6,8 +8,11 @@ function NavLibraryFilter() {
 
     return (
         <div className='navigation--library-filter-container grid'>
-            <div>Playlists</div>
-            <div>Albums</div>
+            <div className="navigation--library-filter-none-container grid">
+                <img className="navigation--library-filter-none-icon" src={CrossGray} alt="cross icon" />
+            </div>
+            <div className="navigation--library-filter-playlists">Playlists</div>
+            <div className="navigation--library-filter-albums">Albums</div>
         </div>
     )
 }
