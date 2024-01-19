3e7f6f8c1e3af6a0f95aad642937399d584dc4a3
Satoki <satokiito@gmail.com>
2024-01-19 01:12:57 +0000

    addded logic to toggle between filters and also a reset button to remove filters

--- src/components/navigation/components/NavLibraryFilter.jsx
+++ src/components/navigation/components/NavLibraryFilter.jsx
@@ -1,18 +1,49 @@
-import { NavLink } from "react-router-dom"
 import CrossGray from "../../../assets/svgs/main-app/cross-gray.svg"
-import CrossBlack from "../../../assets/svgs/main-app/cross-black.svg"
+import { useState } from "react"
 
 import "../styles/nav-library-filter.css"
 
 function NavLibraryFilter() {
 
+    const INITIAL_NAV_FILTER_STATE = [
+        {
+            name: "Playlists",
+            className: "navigation--inactive-library-filter",
+        },{
+            name: "Albums",
+            className: "navigation--inactive-library-filter",
+        }
+    ]
+
+    const [libraryFilter, setLibraryFilter] = useState(INITIAL_NAV_FILTER_STATE)
+
+    const toggleActiveLibraryFilter = (e) => {
+        const updatedFilter = libraryFilter.map(filter => {
+            if (filter.name === e.target.innerText) {
+                return { ...filter, className: "navigation--active-library-filter"}
+            }
+            else {
+                return { ...filter, className: "navigation--inactive-library-filter"}
+            }
+        })
+        setLibraryFilter(updatedFilter)
+    }
+
+    const resetPlaylistFilter = () => [
+        setLibraryFilter(INITIAL_NAV_FILTER_STATE)
+    ]
+
     return (
         <div className='navigation--library-filter-container grid'>
-            <div className="navigation--library-filter-none-container grid">
+            <div className="navigation--library-filter-none-container grid" onClick={() => resetPlaylistFilter()}>
                 <img className="navigation--library-filter-none-icon" src={CrossGray} alt="cross icon" />
             </div>
-            <div className="navigation--library-filter-playlists">Playlists</div>
-            <div className="navigation--library-filter-albums">Albums</div>
+            {libraryFilter.map((filter, index) => (
+                <div className={filter.className} key={`${filter.name}-${index}`} onClick={e => toggleActiveLibraryFilter(e)}>
+                    {filter.name}
+                </div>
+            ))}
+
         </div>
     )
 }
