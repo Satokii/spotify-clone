e0d895ceea8028d8a3778e54f7935090359a099d
Satoki <satokiito@gmail.com>
2024-01-21 23:50:07 +0000

    user playlist page basic structure added

--- src/components/user-playlist/index.jsx
+++ src/components/user-playlist/index.jsx
@@ -1,7 +1,18 @@
+import "./styles/user-playlist.css"
+
 function UserPlaylist() {
 
     return (
-        <section></section>
+        <div className="scrollbar-user-playlist">
+        <section className="user-playlist--container grid">
+            <div>top nav</div>
+            <div>banner</div>
+            <div className="user-playlist--sub-container grid">
+                <div>controls</div>
+                <div>tracks</div>
+            </div>
+        </section>
+        </div>
     )
 }
 
