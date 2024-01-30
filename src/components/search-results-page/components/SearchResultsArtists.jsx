68d025ba1dd90280a3187e6a974001f2a40efd65
Satoki <satokiito@gmail.com>
2024-01-30 19:11:46 +0000

    search results artists base styling added

--- src/components/search-results-page/components/SearchResultsArtists.jsx
+++ src/components/search-results-page/components/SearchResultsArtists.jsx
@@ -1,19 +1,30 @@
 import fixLengthSearch from "../../../shared-functions/fixedLengthSearch";
+import PlayGreen from "../../../assets/svgs/main-app/main-play-btn.svg"
+
+
+import "../styles/search-results-artists.css"
 
 function SearchResultsArtists({ artistResults }) {
   return (
     <div className="search-results-artists--container grid">
       <h3 className="search-results-artists-header">Artists</h3>
-      <ul className="search-results--category-list grid">
+      <ul className="artist--section-list grid">
         {artistResults.map((artist, index) => (
           <li
-            className="search-results--category-item grid"
+            className="artist--section-single-track grid"
             key={`${artist.id}-${index}`}
           >
+            <div className="artist--section-img-outer-container">
+                {artist.images.length ? 
+                  <div className="artist--section-img-container">
+                      <img className="search-results--artists-img" src={artist.images[0].url} alt="artist image" />
+                        <img className="artist--section-play" src={PlayGreen} alt="play button" />
+                      </div>
+                : <div className="search-results--artists-img"></div>
+                }
+            </div>
             <p className="search-results--category-text-bold">{fixLengthSearch(artist.name)}</p>
-            <p className="search-results--category-text">{`Popularity rating: ${artist.popularity}`}</p>
-            {artist.images.length ? <img src={artist.images[0].url} alt={`${artist.name} image`} /> : <div>No Image</div>}
-            <p className="search-results--category-text-other">{`Spotify followers: ${(artist.followers.total).toLocaleString()}`}</p>
+            <p>artist</p>
           </li>
         ))}
       </ul>
