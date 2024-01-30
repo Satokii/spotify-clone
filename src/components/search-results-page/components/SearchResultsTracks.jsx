5c2c449dd039cc3a3c3ba6813c6cf9038869ee6d
Satoki <satokiito@gmail.com>
2024-01-30 17:49:22 +0000

    initial restructure of track search results

--- src/components/search-results-page/components/SearchResultsTracks.jsx
+++ src/components/search-results-page/components/SearchResultsTracks.jsx
@@ -1,22 +1,25 @@
 import fixLengthSearch from "../../../shared-functions/fixedLengthSearch";
 import formatDate from "../../../shared-functions/formatDate";
 
+import "../styles/search-results-tracks.css"
+
 function SearchResultsTracks({ trackTotal, trackResults }) {
 
   return (
-    <div className="search-results--category-container grid">
-      <h3 className="search-results--category-subheader ">Songs</h3>
-      <p className="search-results--category-num-results">{`(Total results: ${trackTotal})`}</p>
-      <ul className="search-results--category-list grid">
+    <div className="search-results-tracks--container grid">
+      <h3 className="search-results-tracks--header">Songs</h3>
+      <ul className="search-results-tracks--list grid">
         {trackResults.map((track, index) => (
           <li
-            className="search-results--category-item grid"
+            className="search-results-tracks--item grid"
             key={`${track.id}-${index}`}
           >
-            <p className="search-results--category-text-bold">{fixLengthSearch(track.name)}</p>
-            <p className="search-results--category-text">{fixLengthSearch(track.artists[0].name)}</p>
             {track.album.images.length ? <img src={track.album.images[0].url} alt={`${track.name} image`} /> : <div>No Image</div>}
-            <p className="search-results--category-text-other">{`Released: ${formatDate(track.album.release_date)}`}</p>
+            <div className="search-results-tracks--item-text">
+              <p className="search-results--category-text-bold">{fixLengthSearch(track.name)}</p>
+              <p className="search-results--category-text">{fixLengthSearch(track.artists[0].name)}</p>
+            </div>
+            <div>time</div>
           </li>
         ))}
       </ul>
