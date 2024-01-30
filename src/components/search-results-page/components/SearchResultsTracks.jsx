24cc1da94516d0b32cdf5770eab6412cd0d25bcc
Satoki <satokiito@gmail.com>
2024-01-30 18:38:18 +0000

    added search result track time

--- src/components/search-results-page/components/SearchResultsTracks.jsx
+++ src/components/search-results-page/components/SearchResultsTracks.jsx
@@ -4,6 +4,7 @@ import GetSearchTrackArtists from "../../../shared-functions/GetSearchTrackArtis
 import GetTrackArtists from "../../../shared-functions/GetTrackArtists";
 GetTrackArtists
 import scrollToTop from "../../../shared-functions/scrollToTop";
+import calcTrackTime from "../../../shared-functions/calcTrackTime";
 
 import "../styles/search-results-tracks.css"
 
@@ -15,15 +16,15 @@ function SearchResultsTracks({ trackTotal, trackResults }) {
       <ul className="search-results-tracks--list grid">
         {trackResults.map((track, index) => (
           <li
-            className="search-results-tracks--item grid"
+            className="search-results-tracks--track grid"
             key={`${track.id}-${index}`}
           >
             {track.album.images.length ? <img src={track.album.images[0].url} alt={`${track.name} image`} /> : <div>No Image</div>}
-            <div className="search-results-tracks--item-text-container grid">
-              <Link className="search-results-tracks--item-name" to={`/album/${track.album.id}/${track.artists[0].id}`} onClick={scrollToTop} >{fixLengthSearch(track.name)}</Link>
-              <p className="search-results-tracks--item-artist">{GetSearchTrackArtists(track)}</p>
+            <div className="search-results-tracks--track-text-container grid">
+              <Link className="search-results-tracks--track-name" to={`/album/${track.album.id}/${track.artists[0].id}`} onClick={scrollToTop} >{fixLengthSearch(track.name)}</Link>
+              <p className="search-results-tracks--track-artist">{GetSearchTrackArtists(track)}</p>
             </div>
-            <div>time</div>
+            <div className="search-results-tracks--track-time">{calcTrackTime(track.duration_ms)}</div>
           </li>
         ))}
       </ul>
