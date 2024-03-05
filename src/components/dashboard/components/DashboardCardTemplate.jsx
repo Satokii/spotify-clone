d8454f3103724de680996bad9f9ca6973352bcc3
Satoki <satokiito@gmail.com>
2024-03-05 18:21:51 +0000

    feat: added card template code to component

--- src/components/dashboard/components/DashboardCardTemplate.jsx
+++ src/components/dashboard/components/DashboardCardTemplate.jsx
@@ -1,7 +1,40 @@
-function DashboardCardTemplate() {
-    return (
-        <section></section>
-    )
+import { Link } from "react-router-dom";
+import scrollToTop from "../../../shared-functions/scrollToTop";
+import PlayGreen from "../../../assets/svgs/main-app/main-play-btn.svg"
+import fixLenthPlistDesc from "../../../shared-functions/fixLengthPlistDesc";
+
+function DashboardCardTemplate({ itemArr }) {
+  return (
+    <div className="dashboard--top-featured-playlists-list grid">
+      {itemArr.map((playlist) => (
+        <Link
+          className="top-featured-playlist--item-container grid"
+          key={playlist.id}
+          to={`/playlist/${playlist.id}`}
+          onClick={scrollToTop}
+        >
+          <div className="top-featured-playlist--img-container">
+            {playlist.images.length ? (
+              <div className="top-featured-playlist--img">
+                <img src={playlist.images[0].url} alt={playlist.name} />
+                <img
+                  className="top-featured-playlist--play"
+                  src={PlayGreen}
+                  alt="play button"
+                />
+              </div>
+            ) : (
+              <div></div>
+            )}
+          </div>
+          <div className="top-featured-playlist--name">{playlist.name}</div>
+          <div className="top-featured-playlist--description">
+            {fixLenthPlistDesc(playlist.description)}
+          </div>
+        </Link>
+      ))}
+    </div>
+  );
 }
 
-export default DashboardCardTemplate
+export default DashboardCardTemplate;
