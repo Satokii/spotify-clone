ca1be56e733346667acbb37929d7e680ce62bab5
Satoki <satokiito@gmail.com>
2024-03-06 17:08:58 +0000

    style: add structure to track details section of sidebar queue

--- src/components/sidebar/components/SidebarNextInQueue.jsx
+++ src/components/sidebar/components/SidebarNextInQueue.jsx
@@ -8,8 +8,8 @@ function SidebarNextInQueue() {
                 <p className="sidebar--queue-title">Next in queue</p>
                 <p className="sidebar--queue-open-queue">Open queue</p>
             </div>
-            <div>
-                <img src={noteIcon} alt="note icon" />
+            <div className="sidebar--queue-track-details-container grid">
+                <img className="sidebar--queue-note-icon" src={noteIcon} alt="note icon" />
                 <img src="" alt="" />
                 <div>
                     <p>track title</p>
