7df19c39c6fff564a62ca46c0fc054d999841475
Satoki <satokiito@gmail.com>
2024-03-06 16:11:44 +0000

    build: separate playlist categories file created

---
+++ src/components/dashboard/playlistCategories.js
@@ -0,0 +1,38 @@
+export const playlistCategories = [
+    {
+        category: "pop",
+        title: "Pop"
+    },
+    {
+        category: "rock",
+        title: "Rock"
+    },
+    {
+        category: "hiphop",
+        title: "Hip-Hop"
+    },
+    {
+        category: "mood",
+        title: "Mood"
+    },
+    {
+        category: "party",
+        title: "Party}"
+    },
+    {
+        category: "kpop",
+        title: "K-Pop"
+    },
+    {
+        category: "indie",
+        title: "Indie"
+    },
+    {
+        category: "throwback",
+        title: "Throwback"
+    },
+    {
+        category: "0JQ5DAqbMKFEZPnFQSFB1T",
+        title: "R&B"
+    }
+]
