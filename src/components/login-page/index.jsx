79ceabde28140db5542b181b85e0c0b3b41835b1
Satoki <satokiito@gmail.com>
2024-03-08 18:34:43 +0000

    feat: add form to login page

--- src/components/login-page/index.jsx
+++ src/components/login-page/index.jsx
@@ -1,8 +1,11 @@
 import axios from "axios"
+import { useState } from "react";
 import { useNavigate } from "react-router-dom";
 
 function LoginPage() {
     const navigate = useNavigate()
+    const [user, setUser] = useState({ username: '', password: '' });
+
 
     const handleLogin = async () => {
         const { data } = await axios.post(`http://localhost:4000/login`,
@@ -24,8 +27,23 @@ function LoginPage() {
         navigate('/spotify-login')
     }
 
+    const handleChange = (e) => {
+        const { name, value } = e.target;
+
+        setUser({
+            ...user,
+            [name]: value
+        });
+    };
+
     return (
-        <section onClick={handleLogin}>Click here to log in</section>
+        <section>
+            <form onSubmit={handleLogin}>
+                <input type="text" name="username" placeholder="Username" value={user.username} onChange={handleChange} />
+                <input type="password" name="password" placeholder="Password" value={user.password} onChange={handleChange} />
+                <button type="submit">Click here to log in</button>
+            </form>  
+        </section>
     )
 }
 
