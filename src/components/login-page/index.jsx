96e90ea1660b9aeace8d8cb0d04f040969dfbb6a
Satoki <satokiito@gmail.com>
2024-03-08 18:55:57 +0000

    feat: render login error message on page if incorrect details entered

--- src/components/login-page/index.jsx
+++ src/components/login-page/index.jsx
@@ -5,23 +5,24 @@ import { useNavigate } from "react-router-dom";
 function LoginPage() {
     const navigate = useNavigate()
     const [user, setUser] = useState({ username: '', password: '' });
+    const [loginMessage, setLoginMessage] = useState('')
 
     const handleLogin = async (e) => {
         e.preventDefault();
 
         try {
             const { data } = await axios.post(`http://localhost:4000/login`,
-            {
-                username: user.username,
-                password: user.password
-            },
-            {
-                headers: { 'Content-Type': 'application/json' },
-            }
+                {
+                    username: user.username,
+                    password: user.password
+                },
+                {
+                    headers: { 'Content-Type': 'application/json' },
+                }
             );
 
             if (!data.token) {
-            throw new Error(data.error)
+                throw new Error(data.error)
             }
         
             localStorage.setItem("login-token", data.token)
@@ -29,7 +30,7 @@ function LoginPage() {
             setUser({ username: '', password: '' })
         }
         catch (err) {
-            console.log(err.response.data.error)
+            setLoginMessage(err.response.data.error)
         }
     }
 
@@ -49,6 +50,7 @@ function LoginPage() {
                 <input type="password" name="password" placeholder="Password" value={user.password} onChange={handleChange} />
                 <button type="submit">Click here to log in</button>
             </form>  
+            {loginMessage && <p>{loginMessage}</p>}
         </section>
     )
 }
