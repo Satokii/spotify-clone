61fdd2d5f9906296be6ae867f2d80cbd545c442a
Satoki <satokiito@gmail.com>
2024-03-08 18:15:58 +0000

    feat: add function to handle login through api call to server

--- src/components/login-page/index.jsx
+++ src/components/login-page/index.jsx
@@ -1,5 +1,29 @@
+import axios from "axios"
+
 function LoginPage() {
 
+    const handleLogin = async () => {
+        const { data } = await axios.post(`http://localhost:4000/login`,
+        {
+            username: 'User',
+            password: 'Password123'
+        },
+        {
+            headers: { 'Content-Type': 'application/json' },
+        }
+        );
+            console.log(data)
+
+        if (!data.token) {
+          throw new Error(data.error)
+        }
+    
+        localStorage.setItem("login-token", data.token)
+    }
+
+    return (
+        <section onClick={handleLogin}>Click here to log in</section>
+    )
 }
 
 export default LoginPage
