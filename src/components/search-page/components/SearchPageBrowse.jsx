cc9daeb04f0598c250b4cabb6daf62ba9a8ef0de
Satoki <satokiito@gmail.com>
2024-03-13 18:50:33 +0000

    feat: add base to search page browse section

--- src/components/search-page/components/SearchPageBrowse.jsx
+++ src/components/search-page/components/SearchPageBrowse.jsx
@@ -3,8 +3,11 @@ import '../styles/search-page-browse.css'
 function SearchPageBrowse() {
 
     return (
-        <div className='search-page-browse--container'>
-            <h2>Browse all</h2>
+        <div className='search-page-browse--container grid'>
+            <h2 className='search-page-browse--header'>Browse all</h2>
+            <section className='search-page-browse--genre-container'>
+                Genre cards
+            </section>
         </div>
     )
 }
