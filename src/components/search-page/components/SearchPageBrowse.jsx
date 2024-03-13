6c179285dee9e78ac4dc0c1cbbb4ef946d8e768c
Satoki <satokiito@gmail.com>
2024-03-13 19:04:33 +0000

    feat: add array of genre objects to map and render on page

--- src/components/search-page/components/SearchPageBrowse.jsx
+++ src/components/search-page/components/SearchPageBrowse.jsx
@@ -2,11 +2,113 @@ import '../styles/search-page-browse.css'
 
 function SearchPageBrowse() {
 
+    const genres = [
+        {
+            genre: "pop",
+            title: "Pop"
+        },
+        {
+            genre: "mood",
+            title: "Mood"
+        },
+        {
+            genre: "hiphop",
+            title: "Hip-Hop"
+        },
+        {
+            genre: "party",
+            title: "Party"
+        },
+        {
+            genre: "",
+            title: "Dance/Electronic"
+        },
+        {
+            genre: "workout",
+            title: "Workout"
+        },
+        {
+            genre: "",
+            title: "Equal"
+        },
+        {
+            genre: "",
+            title: "In the car"
+        },
+        {
+            genre: "",
+            title: "Alternative"
+        },
+        {
+            genre: "",
+            title: "Frequency"
+        },
+        {
+            genre: "indie",
+            title: "Indie"
+        },
+        {
+            genre: "rock",
+            title: "Rock"
+        },
+        {
+            genre: "0JQ5DAqbMKFEZPnFQSFB1T",
+            title: "R&B"
+        },
+        {
+            genre: "throwback",
+            title: "Throwback"
+        },
+        {
+            genre: "",
+            title: "Disney"
+        },
+        {
+            genre: "",
+            title: "Radar"
+        },
+        {
+            genre: "",
+            title: "Country"
+        },
+        {
+            genre: "kpop",
+            title: "K-Pop"
+        },
+        {
+            genre: "",
+            title: "Chill"
+        },
+        {
+            genre: "",
+            title: "Sleep"
+        },
+        {
+            genre: "jazz",
+            title: "Jazz"
+        },
+        {
+            genre: "classical",
+            title: "Classical"
+        },
+        {
+            genre: "anime",
+            title: "Anime"
+        },
+        {
+            genre: "",
+            title: "Metal"
+        },
+        
+    ]
+
     return (
         <div className='search-page-browse--container grid'>
             <h2 className='search-page-browse--header'>Browse all</h2>
-            <section className='search-page-browse--genre-container'>
-                Genre cards
+            <section className='search-page-browse--genre-container grid'>
+                {genres.map((genre, index) => (
+                    <div key={`${genre.title}-${index}`}>{genre.title}</div>
+                ))}
             </section>
         </div>
     )
