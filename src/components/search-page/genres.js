3730b9fda2575f64df54c20916f01bb875f039a6
Satoki <satokiito@gmail.com>
2024-03-13 19:07:14 +0000

    refactor: extract genres array into separate file

---
+++ src/components/search-page/genres.js
@@ -0,0 +1,99 @@
+export const genres = [
+    {
+        genre: "pop",
+        title: "Pop"
+    },
+    {
+        genre: "mood",
+        title: "Mood"
+    },
+    {
+        genre: "hiphop",
+        title: "Hip-Hop"
+    },
+    {
+        genre: "party",
+        title: "Party"
+    },
+    {
+        genre: "",
+        title: "Dance/Electronic"
+    },
+    {
+        genre: "workout",
+        title: "Workout"
+    },
+    {
+        genre: "",
+        title: "Equal"
+    },
+    {
+        genre: "",
+        title: "In the car"
+    },
+    {
+        genre: "",
+        title: "Alternative"
+    },
+    {
+        genre: "",
+        title: "Frequency"
+    },
+    {
+        genre: "indie",
+        title: "Indie"
+    },
+    {
+        genre: "rock",
+        title: "Rock"
+    },
+    {
+        genre: "0JQ5DAqbMKFEZPnFQSFB1T",
+        title: "R&B"
+    },
+    {
+        genre: "throwback",
+        title: "Throwback"
+    },
+    {
+        genre: "",
+        title: "Disney"
+    },
+    {
+        genre: "",
+        title: "Radar"
+    },
+    {
+        genre: "",
+        title: "Country"
+    },
+    {
+        genre: "kpop",
+        title: "K-Pop"
+    },
+    {
+        genre: "",
+        title: "Chill"
+    },
+    {
+        genre: "",
+        title: "Sleep"
+    },
+    {
+        genre: "jazz",
+        title: "Jazz"
+    },
+    {
+        genre: "classical",
+        title: "Classical"
+    },
+    {
+        genre: "anime",
+        title: "Anime"
+    },
+    {
+        genre: "",
+        title: "Metal"
+    },
+    
+]
