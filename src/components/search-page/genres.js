3be90948bd87ba4d08d70c78541fd68a0fc6e7c5
Satoki <satokiito@gmail.com>
2024-03-14 14:31:22 +0000

    feat: add image links to genre array

--- src/components/search-page/genres.js
+++ src/components/search-page/genres.js
@@ -2,122 +2,146 @@ export const genres = [
     {
         genre: "pop",
         title: "Pop",
-        background: "rgb(20, 138, 8)"
+        background: "rgb(20, 138, 8)",
+        img: "https://i.scdn.co/image/ab67fb8200005cafa862ab80dd85682b37c4e768"
     },
     {
         genre: "mood",
         title: "Mood",
-        background: "rgb(225, 17, 140)"
+        background: "rgb(225, 17, 140)",
+        img: "https://i.scdn.co/image/ab67fb8200005caf271f9d895003c5f5561c1354"
     },
     {
         genre: "hiphop",
         title: "Hip-Hop",
-        background: "rgb(80, 55, 80)"
+        background: "rgb(80, 55, 80)",
+        img: "https://i.scdn.co/image/ab67fb8200005caf9e3dea60be755ccd97b7351f"
     },
     {
         genre: "party",
         title: "Party",
-        background: "rgb(83, 122, 161)"
+        background: "rgb(83, 122, 161)",
+        img: "https://i.scdn.co/image/ab67fb8200005cafcbf80f8ea695536eace4fd2c"
     },
     {
         genre: "0JQ5DAqbMKFHOzuVTgTizF",
         title: "Dance/Electronic",
-        background: "rgb(216, 64, 0)"
+        background: "rgb(216, 64, 0)",
+        img: "https://i.scdn.co/image/ab67fb8200005cafdfdaac1cf9574a196ca25196"
     },
     {
         genre: "workout",
         title: "Workout",
-        background: "rgb(119, 119, 119)"
+        background: "rgb(119, 119, 119)",
+        img: "https://i.scdn.co/image/ab67706f000000029249b35f23fb596b6f006a15"
     },
     {
         genre: "equal",
         title: "Equal",
-        background: "rgb(5, 105, 82)"
+        background: "rgb(5, 105, 82)",
+        img: "https://i.scdn.co/image/ab67fb8200005caf9ed6e364e8839210dc4dbff7"
     },
     {
         genre: "0JQ5DAqbMKFIRybaNTYXXy",
         title: "In the car",
-        background: "rgb(45, 70, 185)"
+        background: "rgb(45, 70, 185)",
+        img: "https://i.scdn.co/image/ab67fb8200005caf341265d27be641506e56c346"
     },
     {
         genre: "alternative",
         title: "Alternative",
-        background: "rgb(30, 50, 100)"
+        background: "rgb(30, 50, 100)",
+        img: "https://i.scdn.co/image/ab67fb8200005caf132cf10d0b5d2344e004eff8"
     },
     {
         genre: "frequency",
         title: "Frequency",
-        background: "rgb(13, 115, 236)"
+        background: "rgb(13, 115, 236)",
+        img: "https://i.scdn.co/image/ab67fb8200005caf9571e2fc2a85d91eda472f04"
     },
     {
         genre: "indie",
         title: "Indie",
-        background: "rgb(71, 125, 149)"
+        background: "rgb(71, 125, 149)",
+        img: "https://i.scdn.co/image/ab67fb8200005caf8f586b90d9dfa138bbc3dcb6"
     },
     {
         genre: "rock",
         title: "Rock",
-        background: "rgb(225, 51, 0)"
+        background: "rgb(225, 51, 0)",
+        img: "https://i.scdn.co/image/ab67fb8200005caf9ca6c68a1b29f47895e611e7"
     },
     {
         genre: "0JQ5DAqbMKFEZPnFQSFB1T",
         title: "R&B",
-        background: "rgb(186, 93, 7)"
+        background: "rgb(186, 93, 7)",
+        img: "https://i.scdn.co/image/ab67fb8200005caff4e38be86ca48a3b10884ae3"
     },
     {
         genre: "throwback",
         title: "Throwback",
-        background: "rgb(141, 103, 171)"
+        background: "rgb(141, 103, 171)",
+        img: "https://i.scdn.co/image/ab67fb8200005cafded6f7cbf0d9edcd92776473"
     },
     {
         genre: "disney",
         title: "Disney",
-        background: "rgb(13, 115, 236)"
+        background: "rgb(13, 115, 236)",
+        img: "https://i.scdn.co/image/ab67fb8200005caf78c17dc5631c053fd95f31bb"
     },
     {
         genre: "radar",
         title: "Radar",
-        background: "rgb(165, 103, 82)"
+        background: "rgb(165, 103, 82)",
+        img: "https://i.scdn.co/image/ab67fb8200005cafe760702de052fbefea27064a"
     },
     {
         genre: "country",
         title: "Country",
-        background: "rgb(216, 64, 0)"
+        background: "rgb(216, 64, 0)",
+        img: "https://i.scdn.co/image/ab67fb8200005cafc0d2222b4c6441930e1a386e"
     },
     {
         genre: "kpop",
         title: "K-Pop",
-        background: "rgb(20, 138, 8)"
+        background: "rgb(20, 138, 8)",
+        img: "https://i.scdn.co/image/ab67fb8200005caf013ee3c983e6f60bf28bad5a"
     },
     {
         genre: "chill",
         title: "Chill",
-        background: "rgb(216, 64, 0)"
+        background: "rgb(216, 64, 0)",
+        img: "https://i.scdn.co/image/ab67fb8200005caf47e942f5bea637f4f4760170"
     },
     {
         genre: "sleep",
         title: "Sleep",
-        background: "rgb(30, 50, 100)"
+        background: "rgb(30, 50, 100)",
+        img: "https://i.scdn.co/image/ab67706f00000002b70e0223f544b1faa2e95ed0"
     },
     {
         genre: "jazz",
         title: "Jazz",
-        background: "rgb(119, 119, 119)"
+        background: "rgb(119, 119, 119)",
+        img: "https://i.scdn.co/image/ab67fb8200005cafe289743024639ea8f202364d"
     },
     {
         genre: "classical",
         title: "Classical",
-        background: "rgb(125, 75, 50)"
+        background: "rgb(125, 75, 50)",
+        img: "https://i.scdn.co/image/ab67fb8200005caf12809992dfc5b318892ea07b"
     },
     {
         genre: "anime",
         title: "Anime",
-        background: "rgb(228, 29, 99)"
+        background: "rgb(228, 29, 99)",
+        img: "https://i.scdn.co/image/ab67706f00000002c19c5f13f8b3ff2d73ff00bc"
     },
     {
         genre: "metal",
         title: "Metal",
-        background: "rgb(233, 20, 41)"
+        background: "rgb(233, 20, 41)",
+        img: "https://i.scdn.co/image/ab67fb8200005cafefa737b67ec51ec989f5a51d"
     },
     
 ]
