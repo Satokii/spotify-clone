f7ae7eabd7f02b252df48cbe6d571a41a4424c14
Satoki <satokiito@gmail.com>
2024-03-15 13:02:50 +0000

    feat: add api call to retrieve track tally data and render on page

--- src/components/top-played/index.jsx
+++ src/components/top-played/index.jsx
@@ -1,6 +1,33 @@
+import { useEffect, useState } from "react"
+import axios from "axios"
+
 function TopPlayed() {
+
+    const [tracks, setTracks] = useState([])
+
+    useEffect(()=> {
+        const getTrackData = async () => {
+            const { data } = await axios.get(`http://localhost:4000/tracks`,
+            {
+                headers: { 'Content-Type': 'application/json' },
+            }
+            );
+            setTracks(data.tracks)
+        }
+        getTrackData()
+    }, [])
+    console.log('tracks', tracks)
+
     return (
-        <section></section>
+        <section>
+            <h2>What you've been listening to</h2>
+                {tracks.map((track, index) => 
+                    <div key={`${track.id}-${index}`}>
+                        <p>{track.track_name}</p>
+                        <p>{track.tally}</p>
+                    </div>
+                )}
+        </section>
     )
 }
 
